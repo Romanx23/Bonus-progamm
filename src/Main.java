de2d1074a71aa72363aecad8ae0aff08aad2bccc
Роман Панченко <romanx23@yandex.ru>
2022-08-08 21:10:34 +0300

    Miles modified

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int amount = 7530;
-        int percent = 20;
-        int bonus = amount / percent;
-        System.out.println("Your bonus is: "+ bonus +" miles");
-
-
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной милли
-
-        // Рассчитываете количество бонусных милль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
+        BonusMilesService service = new BonusMilesService();
+        int price = 7420;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
